23fce7822bb5bfe81dc704a2c4c4dee8a76df6fd
wbg1991 <qudrl345@gmail.com>
2024-04-08 21:09:56 +0900

    create tablet and desktop scaffold's body

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_ui/constants.dart';
 
+import '../util/my_box.dart';
+import '../util/my_tile.dart';
+
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
 
@@ -20,7 +23,43 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
           myDrawer,
 
           // rest of body
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                // 4 box on the top
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                        itemCount: 4,
+                        gridDelegate:
+                        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                        itemBuilder: (context, index) {
+                          return const MyBox();
+                        }
+                    ),
+                  ),
+                ),
 
+                // tiles below it
+                Expanded(child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return MyTile();
+                    }
+                )),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Column(
+                children: [
+                  Expanded(child: Container(color: Colors.pink,),),
+                ],
+              )
+          )
         ],
       ),
     );
--- lib/responsive/tablet_scaffold.dart
+++ lib/responsive/tablet_scaffold.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_ui/constants.dart';
 
+import '../util/my_box.dart';
+import '../util/my_tile.dart';
+
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({super.key});
 
@@ -15,6 +18,33 @@ class _TabletScaffoldState extends State<TabletScaffold> {
       backgroundColor: myDefaultBackground,
       appBar: myAppBar,
       drawer: myDrawer,
+      body: Column(
+        children: [
+          // 4 box on the top
+          AspectRatio(
+            aspectRatio: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                  itemCount: 4,
+                  gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                  itemBuilder: (context, index) {
+                    return const MyBox();
+                  }
+              ),
+            ),
+          ),
+
+          // tiles below it
+          Expanded(child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return MyTile();
+              }
+          ))
+        ],
+      ),
     );
   }
 }
